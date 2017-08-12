7d0989916c50426da85a5931e767b07c89ded364
John Chleborad <jchleborad@gmail.com>
2017-08-12 14:19:33 -0700

    hardware list logging

--- js/less.js
+++ js/less.js
@@ -1,63 +1,45 @@
-  /* TODO: Move To js file */
-  /*********************************************************
-   *
-   *   Test Checkboxes/Radio Buttons Script
-   *
-   *********************************************************/
-   // loop through each form and handle submit event
-  //  for (var i=0;i<document.forms.length;i++) {
-  //    var form = document.forms[i];
-  //    form.addEventListener('submit', function(e){
-  //      e.preventDefault();
-  //      var results = '';
-  //     // loop through all checkboxes to see if checked
-  //     var checkboxes = form.querySelectorAll("input[type='checkbox']");
-  //     for(var k = 0; k < checkboxes.length; k++) {
-  //       results += checkboxes[k].id + '=' + checkboxes[k].checked + '\n';
-  //     }
-  //     // loop through all radio butotns to display value
-  //     var radiobuttons = form.querySelectorAll("input[type='radio']:checked");
-  //     for(var m = 0; m < radiobuttons.length; m++) {
-  //       results += radiobuttons[m].name + '=' + radiobuttons[m].value + '\n';
-  //     }
-  //     // alert results
-  //     alert(results);
-  //   });
-  // }
+//HARDWARE OPTIONS TO CONSOLE
+$(
+  function() {
+    $("#save").click(function() {
+        $(':input:checked',"#hardwareOptions").each(function() {
+            if ($(this))
+              console.log( $(this).attr('name') + ': ' + $(this).attr('value'));
+              // alert ( $(this).attr('name') + ': ' + $(this).attr('value'));
+          });
 
+      });
 
-//List-Array of store products
-// var productList = [
-//   'RoboDog', 'ButlerBot', 'MegaMan', 'HelloRobot', 'Inteli3', 'Inteli5', 'Inteli7', 'Ram8G', 'Ram16G', 'VRHelmet', '3DScreen', 'StereoSpeakers', 'BlueToothKeyboard', 'WirelessMouse', 'PortableMonitor', 'RoboSkin1', 'RoboSkin2', 'RoboCase'
-// ];
+  });
 
-// Robot search filter
-$('#searchbox').keyup(function(){
-   var valThis = $(this).val().toLowerCase();
-    if(valThis == ""){
-        $('.navList > i').show();
-    } else {
-        $('.navList > li').each(function(){
-            var text = $(this).text().toLowerCase();
-            (text.indexOf(valThis) >= 0) ? $(this).show() : $(this).hide();
-        });
-   };
-});
-
-// Sample Search Filter
-function myFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-
-        }
+//ROBOT SEARCH FILTER
+(function ($) {
+  jQuery.expr[':'].Contains = function(a,i,m){
+    return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
+  };
+  function listFilter(header, list) {
+    $('.searchbox')
+      .change( function () {
+      var filter = $(this).val();
+      if(filter) {
+        $(list).find("a:not(:Contains(" + filter + "))").parent().slideUp();
+        $(list).find("a:Contains(" + filter + ")").parent().slideDown();
+      } else {
+        $(list).find("li").slideDown();
+      }
+      return false;
+    })
+    .keyup( function () {
+      $(this).change();
+    });
     }
-}
+    $(function () {
+      listFilter($("#header"), $("#list"));
+    });
+  }(jQuery));
+
+
+  //List-Array of store products
+  // var productList = [
+  //   'RoboDog', 'ButlerBot', 'MegaMan', 'HelloRobot', 'Inteli3', 'Inteli5', 'Inteli7', 'Ram8G', 'Ram16G', 'VRHelmet', '3DScreen', 'StereoSpeakers', 'BlueToothKeyboard', 'WirelessMouse', 'PortableMonitor', 'RoboSkin1', 'RoboSkin2', 'RoboCase'
+  // ];
