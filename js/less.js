6373332f7dd22b1b483d442ac47051528bb4d9f2
John Chleborad <jchleborad@gmail.com>
2017-08-18 21:00:45 -0700

    bootstrap

--- js/less.js
+++ js/less.js
@@ -1,47 +1,42 @@
 //HARDWARE OPTIONS TO CONSOLE
 $(
   function() {
-    var list="";
+    var list = "";
     $("#save").click(function() {
-        $(':input:checked',"#hardwareOptions").each(function() {
-            if ($(this))
-              console.log( $(this).attr('name') + ': ' + $(this).attr('value'));
-              list += $(this).attr('name') + ': ' + $(this).attr('value')  + "\n";
-          });
-          alert ("Congratulations! You have selected the following configuration for your new computer!" + "\n"  + "\n" + list);
-
+      $(':input:checked', "#hardwareOptions").each(function() {
+        if ($(this))
+          console.log($(this).attr('name') + ': ' + $(this).attr('value'));
+        list += $(this).attr('name') + ': ' + $(this).attr('value') + "\n";
       });
+      alert("Congratulations! You have selected the following configuration for your new computer!" + "\n" + "\n" + list);
+
+    });
 
   });
 
 //ROBOT SEARCH FILTER
-(function ($) {
-  jQuery.expr[':'].Contains = function(a,i,m){
-    return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
+(function($) {
+  jQuery.expr[':'].Contains = function(a, i, m) {
+    return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
   };
+
   function listFilter(header, list) {
     $('.searchbox')
-      .change( function () {
-      var filter = $(this).val();
-      if(filter) {
-        $(list).find("a:not(:Contains(" + filter + "))").parent().slideUp();
-        $(list).find("a:Contains(" + filter + ")").parent().slideDown();
-      } else {
-        $(list).find("li").slideDown();
-      }
-      return false;
-    })
-    .keyup( function () {
-      $(this).change();
-    });
-    }
-    $(function () {
-      listFilter($("#header"), $("#list"));
-    });
-  }(jQuery));
-
-
-  //List-Array of store products
-  // var productList = [
-  //   'RoboDog', 'ButlerBot', 'MegaMan', 'HelloRobot', 'Inteli3', 'Inteli5', 'Inteli7', 'Ram8G', 'Ram16G', 'VRHelmet', '3DScreen', 'StereoSpeakers', 'BlueToothKeyboard', 'WirelessMouse', 'PortableMonitor', 'RoboSkin1', 'RoboSkin2', 'RoboCase'
-  // ];
+      .change(function() {
+        var filter = $(this).val();
+        if (filter) {
+          $(list).find("a:not(:Contains(" + filter + "))").parent().slideUp();
+          $(list).find("a:Contains(" + filter + ")").parent().slideDown();
+        } else {
+          $(list).find("li").slideDown();
+        }
+        return false;
+      })
+      .keyup(function() {
+        $(this).change();
+      });
+  }
+  $(function() {
+    listFilter($("#header"), $("#list"));
+  });
+}(jQuery));
